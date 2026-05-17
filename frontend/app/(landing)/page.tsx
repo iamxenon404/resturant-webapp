4f2aa0a2ad748671a628f4ec0dd9f0e9021297e4
xenon <demiladeafuape74@gmail.com>
2026-05-17 20:08:56 +0100

    comment

--- frontend/app/(landing)/page.tsx
+++ frontend/app/(landing)/page.tsx
@@ -3,7 +3,7 @@ import Hero from './LD/Hero';
 import About from './LD/About';
 import Menu from './LD/Menu';
 import Contact from './LD/Contact';
-import Footer from './LD/Footer';
+// import Footer from './LD/Footer';
 
 export default function LandingPage() {
   return (
