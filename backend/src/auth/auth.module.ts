dce366de580c848f0c74e904fc57db4e273a3a6f
xenon <demiladeafuape74@gmail.com>
2026-05-18 18:50:42 +0100

    re imported auth service in auth module

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+// import { AuthService } from './auth.service';
 import { SupabaseJwtStrategy } from './jwt.strategy';
+import { AuthService } from './auth.service';
 
 @Module({
   controllers: [AuthController],
